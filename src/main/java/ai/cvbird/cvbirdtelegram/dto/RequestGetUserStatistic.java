dbdabb25602e7e55b9e60bc4a411cc5f2214ef3a
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2024-05-25 14:20:26 +0300

    feat: created Telergam bot

---
+++ src/main/java/ai/cvbird/cvbirdtelegram/dto/RequestGetUserStatistic.java
@@ -0,0 +1,10 @@
+package ai.cvbird.cvbirdtelegram.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestGetUserStatistic {
+    String telegramId;
+}
---
+++ src/test/java/ai/cvbird/cvbirdtelegram/CvbirdTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.cvbird.cvbirdtelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvbirdTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
