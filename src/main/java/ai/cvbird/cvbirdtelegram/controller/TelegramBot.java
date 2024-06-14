c5fcafc825c45cd3534a82b02c2a599f622d5c76
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2024-06-14 09:43:34 +0300

    feat: Refactoring. Release.

--- src/main/java/ai/cvbird/cvbirdtelegram/controller/TelegramBot.java
+++ src/main/java/ai/cvbird/cvbirdtelegram/controller/TelegramBot.java
@@ -10,12 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.*;
 import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.api.objects.webapp.WebAppData;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -36,25 +36,20 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Autowired
     AIServiceClient aiServiceClient;
 
-    @Autowired
-    StatisticConverter statisticConverter;
-
     public final String START_C = "/start";
-    public final String HELP_C = "/help";
-    public final String HELLO_C = "/hello";
-    public final String REGISTRATION_C = "/registration";
-    public final String ME_C = "/me";
-    public final String COMMANDS_M = "Commands";
-    public final String VACANCIES_M = "Vacancies";
-    public final Integer ONE = 1;
     public final String JOB_RESPONSE = "job_response";
+    public final String FIRST_MESSAGE = "tg://user?id=895038310 Hello! Welcome to CVBird — your personal job search assistant!\n We use cutting-edge artificial intelligence technology to analyze your resume and find the most suitable jobs around the world.\n" +
+            "\n" +
+            "To get started, go to our mini-app and upload your resume. Just click the button below and we will help you find your dream job!\n" +
+            "\n" +
+            "If you have any questions or need help, don't hesitate to contact me. Good luck in your job search!\n\n";
 
     //public TelegramBot(@Value("${tg.bot.token}") String botToken) {
     //    super(botToken);
     //}
 
     public TelegramBot() {
-        super(System.getenv("TG_BOT_TOKEN"));
+        super("6986236750:AAEv0VeXfx3_e3QQuFIKNnbuMgyr7m3_dFA");
     }
 
     @Override
@@ -66,30 +61,22 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(@NotNull Update update) {
         if(update.hasMessage()){
             Message message = update.getMessage();
-            User user = message.getFrom();
-            User forwardUser = message.getForwardFrom();
             WebAppData webAppData = message.getWebAppData();
 
             if (webAppData != null) {
                 webAppDataHandle(webAppData);
             }
-
-
-            if(message.hasText() || message.hasDocument()){
-                documentHandle(message);
+            if (message.hasText() || message.hasDocument()){
                 messageHandle(message);
             }
         }
     }
-
-    private void documentHandle(Message message) {}
 
     private void webAppDataHandle(WebAppData webAppData) {
         String data = webAppData.getData();
         String buttonText = webAppData.getButtonText();
         if (JOB_RESPONSE.equals(buttonText)) {
             JSONObject obj = new JSONObject(data);
-            String employerId  = obj.getString("employer_id");
             String applicantTelegramId = obj.getString("applicant_telegram_id");
             String jobId = obj.getString("job_id");
 
@@ -127,7 +114,6 @@ public class TelegramBot extends TelegramLongPollingBot {
                                 }
                             }
                         }
-
                         deleteFile(path);
                     }
                 }
@@ -137,84 +123,16 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     private void messageHandle(Message message) {
         String text = message.getText();
-
-        RequestGetUserStatistic requestGetUserStatistic = new RequestGetUserStatistic();
-        requestGetUserStatistic.setTelegramId(message.getFrom().getId().toString());
         log(message.getFrom().getFirstName(),message.getFrom().getLastName(), message.getFrom().getId().toString(), message.getText());
-        if (cvBirdSiteClient.getUserStatistic(requestGetUserStatistic) == null) {
-            saveStatistic(message);
-        }
-        if (text != null) {
-            SendPhoto sendPhoto = null;
-            SendMessage sendMessage = null;
-            switch (text) {
-                case HELLO_C:
-                case REGISTRATION_C:
-                case ME_C:
-                    sendMessage = meCommand(message);
-                    break;
-                case VACANCIES_M:
-                case START_C:
-                    sendMessage = startCommand(message);
-                    break;
-                case COMMANDS_M:
-                    sendMessage = getCommands(message);
-                    break;
-                default:
-                    sendPhoto = defaultCommand(message);
-                    break;
-            }
-            try {
-                if (sendMessage != null) {
-                    execute(sendMessage);
-                } else if ( sendPhoto != null) {
-                    execute(sendPhoto);
-                }
 
+        if (START_C.equals(text)) {
+            SendPhoto sendPhoto = defaultCommand(message);
+            try {
+                execute(sendPhoto);
             } catch (TelegramApiException e) {
                 throw new RuntimeException(e);
             }
         }
-    }
-
-    private SendMessage startCommand(Message message) {
-
-       // ReplyKeyboardMarkup replyKeyboardMarkup = getMainMenuKeyboard(language);
-
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.enableMarkdown(true);
-        sendMessage.enableHtml(true);
-        sendMessage.setReplyMarkup(getKeyboardMarkup());
-      //  sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        sendMessage.setText("<b>bold</b>, <strong>bold</strong>\n" +
-                "<i>italic</i>, <em>italic</em>\n" +
-                "<u>underline</u>, <ins>underline</ins>\n" +
-                "<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>\n" +
-                "<span class=\"tg-spoiler\">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>\n" +
-                "<b>bold <i>italic bold <s>italic bold strikethrough <span class=\"tg-spoiler\">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>\n" +
-                "<a href=\"http://www.example.com/\">inline URL</a>\n" +
-                "<a href=\"tg://user?id=123456789\">inline mention of a user</a>\n" +
-                "<tg-emoji emoji-id=\"5368324170671202286\">\uD83D\uDC4D</tg-emoji>\n" +
-                "<code>inline fixed-width code</code>\n" +
-                "<pre>pre-formatted fixed-width code block</pre>\n" +
-                "<pre><code class=\"language-python\">pre-formatted fixed-width code block written in the Python programming language</code></pre>\n" +
-                "<blockquote>Block quotation started\\nBlock quotation continued\\nThe last line of the block quotation</blockquote>" + message.getText());
-        return  sendMessage;
-    }
-
-    private SendMessage getCommands(Message message) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.enableMarkdown(true);
-        sendMessage.enableHtml(true);
-        sendMessage.setReplyMarkup(getKeyboardMarkup());
-        //  sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        sendMessage.setText("/start\n" +
-                "/help\n" +
-                "/me\n" +
-                "/registration\n");
-        return  sendMessage;
     }
 
     private SendPhoto defaultCommand(Message message) {
@@ -230,7 +148,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         try {
             SendPhoto sendPhoto = new SendPhoto();
             sendPhoto.setChatId(message.getChatId().toString());
-            sendPhoto.setCaption("Your ChatID :  " + message.getChatId().toString());
+            sendPhoto.setParseMode(ParseMode.HTML);
+            sendPhoto.setCaption(FIRST_MESSAGE + "<b>Your ChatID : " + message.getChatId().toString() +"</b>");
             sendPhoto.setReplyMarkup(keyboard);
             File file = new File("tmp_logo.png");
             InputStream inputStream = new ClassPathResource("cvbird_logo.png").getInputStream();
@@ -241,43 +160,6 @@ public class TelegramBot extends TelegramLongPollingBot {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private SendMessage meCommand(Message message) {
-        String me = cvBirdSiteClient.userInfo();
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.enableMarkdown(true);
-        sendMessage.enableHtml(true);
-        //  sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        sendMessage.setText(me);
-        return  sendMessage;
-    }
-
-    private ReplyKeyboardMarkup getKeyboardMarkup() {
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-        // Create the keyboard (list of keyboard rows)
-        List<KeyboardRow> keyboard = new ArrayList<>();
-        // Create a keyboard row
-        KeyboardRow row = new KeyboardRow();
-        // Set each button, you can also use KeyboardButton objects if you need something else than text
-        row.add("Vacancies");
-        row.add("Commands");
-        keyboard.add(row);
-
-        // Set the keyboard to the markup
-        keyboardMarkup.setKeyboard(keyboard);
-
-        keyboardMarkup.setResizeKeyboard(true);
-        //keyboardMarkup.setIsPersistent(true);
-
-        return keyboardMarkup;
-    }
-
-    private void saveStatistic(Message message) {
-        StatisticDTO statisticDTO =  statisticConverter.fromUser(message.getFrom());
-        String req = cvBirdSiteClient.saveStatistic(statisticDTO);
-        System.out.println(req);
     }
 
     private void log(String first_name, String last_name, String user_id, String txt) {
@@ -305,5 +187,4 @@ public class TelegramBot extends TelegramLongPollingBot {
         File file = new File(path);
         file.delete();
     }
-
 }
